fb1146908c535ab2336c6ffde91f18437d9d38e4
bandilem-git <u24675394@tuks.co.za>
2026-04-02 10:39:58 +0200

    Base Logger implemented

--- baselogger.h
+++ baselogger.h
@@ -1,30 +1,23 @@
 #ifndef BASELOGGER_H
 #define BASELOGGER_H
 
+#include "log.h"
 #include <iostream>
 #include <string>
 #include <chrono>
 #include <thread>
 #include <mutex>
 
-enum SEVERITY{
-    LOW,
-    MED,
-    HIGH
-};
-
-
 
 class BaseLogger{
     private:
-    std::string targetFile;//stores the file that we are logging into
-    SEVERITY severity; //
-    std::string description;
-    std::mutex mutualExclusion;
-    
-    public:
-    virtual void logSuccess(std::string x) = 0;
+        std::string targetFile; //stores the file that we are logging into
+        std::string desc; //description, no need to format alr done with log 
+        std::mutex loggerLock; // each class has its own lock
+        //TODO: add subscribe
 
+    public:
+    virtual void onEvent(Log log) = 0;
 };
 
 #endif
---
+++ ctrllogger.h
@@ -0,0 +1,13 @@
+#ifndef CTRLLOGGER_H
+#define CTRLLOGGER_H
+
+#include "baselogger.h"
+
+class ControlLogger : public BaseLogger{
+    private:
+    
+    public:
+    
+};
+
+#endif
