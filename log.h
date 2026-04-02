198da4bb0a8baeb887272ab227fd70daf9db31a5
bandilem-git <u24675394@tuks.co.za>
2026-04-02 16:18:15 +0200

    Log completed

--- log.h
+++ log.h
@@ -5,11 +5,15 @@
 #include <iostream>
 #include <string>
 #include <vector> 
+#include <time.h>
+#include <ctime>
 
 enum STATUS{
     ERROR,
     LOG,
-    TEST
+    TEST,
+    STARTUP,
+    SHUTDOWN
 };
 
 class Log{ 
@@ -17,13 +21,19 @@ class Log{
         STATUS status; // log status either ERROR LOG TEST
         std::chrono::steady_clock::time_point currTimePoint = std::chrono::steady_clock::now();// time point
         std::string desc;// one line description of action
+        std::string statusToString();
+        std::string dateAsString();
+        std::string timeAsString();
+        time_t parseDateTime(const char* datetimeString, const char* format);
+        std::string DateTime(time_t time, const char* format);
+
     public:
         Log(STATUS status, std::string desc); //sets the relevant variables
         STATUS getSTATUS();
         std::chrono::steady_clock::time_point getCurrentTimePoint();
         std::string getDescription();
         std::string getFormattedData(); // example format below
-        //[STATUS] [(DD/MM/YY)](HH:M:SS) - threadID: "DESCRIPTION"
+        //[STATUS] [(DD/MM/YY)](HH:M:SS) - "DESCRIPTION"
         //each of them provide (BYTES SENDING FROM A TO B B TO A ETC WHATEVER)
 };
 
